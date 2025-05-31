bac891f61dcebbbd23c40598d48da2c32638049a
Joaco51976 <joaquincordini62@gmail.com>
2025-05-31 14:06:52 -0300

    Add files via upload

---
+++ Ej 3 - Patentes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patentes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int numero = rnd.Next(0,26 * 26 * 26 * 1000);
+
+            int letras = numero / 1000;
+            int numeros = numero % 1000;
+
+            int letra1 = letras / (26 * 26);
+            int letra2 = (letras / 26) % 26;
+            int letra3 = letras % 26;
+
+            char c1 = (char)('A' + letra1);
+            char c2 = (char)('A' + letra2);
+            char c3 = (char)('A' + letra3);
+
+            string resultado = $"{c1}{c2}{c3}-{numeros:D3}";
+
+            Console.WriteLine($"La Patente generada es: {resultado}");
+        }
+    }
+}
